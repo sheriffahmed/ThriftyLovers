a4a11cff8bf4200e38bef5299a06e893424bbb51
ninja-nette <janette.correa@ac.c4q.nyc>
2018-03-18 16:40:58 -0400

    changed month selection option from numbers to words

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -16,18 +16,18 @@ class Registration extends React.Component {
                 <br/>
                 Date of Birth:  <select>
                                     <option>Month</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                    <option>11</option>
-                                    <option>12</option>
+                                    <option>January</option>
+                                    <option>February</option>
+                                    <option>March</option>
+                                    <option>April</option>
+                                    <option>May</option>
+                                    <option>June</option>
+                                    <option>July</option>
+                                    <option>August</option>
+                                    <option>September</option>
+                                    <option>October</option>
+                                    <option>November</option>
+                                    <option>December</option>
                                 </select>
                                 <select>
                                     <option>Day</option>
