8dfe4772bca179a0159716a7973f6bc2312168e6
AJ <atibanurse@ac.c4q.nyc>
2018-03-19 22:43:57 -0400

    Navbar testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import BudgetPage from './components/BudgetPage'
 import LandingPage from './components/LandingPage'
 import Registration from './components/Registration'
 import Messages from './components/Messages'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 
 class App extends Component {
@@ -18,15 +19,48 @@ class App extends Component {
     return (
       //Edit App.css for style
       <div className="App">
-        
+        <Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">React-Bootstrap</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href="#">
+        Link
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link
+      </NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Link Right
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link Right
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>;
         {/*Nav specifically for testing, but can be implimented later. Dont delete*/}
-        <nav>
+        
+        {/* <nav className="navbar navbar-default">
           <Link to='/'> Home </Link> {' '}
           <Link to='/signup'> Signup </Link> {' '}
           <Link to='/budget'> Budget </Link> {' '}
           <Link to='/user/:user'> User </Link> {' '}
           <Link to='/user/:user/messages'> Messages </Link>
-        </nav>
+        </nav> */}
         <Switch>
           <Route exact path='/' component={LandingPage}/>
           <Route path='/signup' component={Registration} />
